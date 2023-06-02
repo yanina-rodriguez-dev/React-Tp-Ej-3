43b9ee2f299e05df5d87e1befd03bc9953a1cd51
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-06-01 22:09:51 -0300

    agrego mensaje inciial para modificarlo luego

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
+import './App.css';
+import Titulos from './components/Titulos';
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   
 
   return (
     <>
+     <Titulos nombre="My friend"></Titulos>
     </>
   )
 }
 
 export default App
+
---
+++ src/components/Titulos.jsx
@@ -0,0 +1,9 @@
+const Titulos = ({nombre}) => {
+    return (
+        <section className="container">
+            <h1 className="mt-5">Hello {nombre}</h1>
+        </section>
+    );
+};
+
+export default Titulos;
